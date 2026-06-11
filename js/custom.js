fc1272d9d2f9a137ffd3514fb78aaa96d27dc41b
1NS <ins16mail@gmail.com>
2026-06-11 20:48:14 +0300

    init commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+/*!
+ * Custom scripts
+ */
+
+// <!-- sortablejs -->
+// new Sortable(document.querySelector('.connectedSortable'), {
+//   group: 'shared',
+//   handle: '.card-header',
+// });
+// const cardHeaders = document.querySelectorAll('.connectedSortable .card-header');
+// cardHeaders.forEach((cardHeader) => {
+//   cardHeader.style.cursor = 'move';
+// });
