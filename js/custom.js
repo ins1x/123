8a9d8726709c6f282469058fe5f850408563a048
1NS <ins16mail@gmail.com>
2026-07-09 11:53:03 +0300

    Update bars and add back to top

--- js/custom.js
+++ js/custom.js
@@ -22,6 +22,7 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+// End: Bootstrap utils
 
 // Locale checker
 const userLocale = navigator.language || navigator.userLanguage;
@@ -29,3 +30,26 @@ const ruLocale = 'ru';
 if (!userLocale.startsWith(ruLocale)) {
   document.getElementsByTagName("BODY")[0].style.display = "none";
 }
+// End:Local checker
+
+// Back to top button
+let backToTopBtn = document.getElementById("btn-back-to-top");
+// On scroll event
+window.onscroll = function () {
+  scrollFunction();
+};
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+backToTopBtn.addEventListener("click", backToTop);
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// End:Back to top button
